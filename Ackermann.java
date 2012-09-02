a7c9931384a579a8de058bba082fd677a20d20b5
Code Ass <aycabta@gmail.com>
2012-09-03 01:38:05 +0900

    implemented for Java that contains compliant and while loop versions

---
+++ Ackermann.java
@@ -0,0 +1,85 @@
+import java.math.BigInteger;
+import java.util.ArrayList;
+
+abstract class AckermannBase {
+	abstract BigInteger Ack(BigInteger m, BigInteger n);
+}
+
+class Ackermann extends AckermannBase {
+	BigInteger Ack(BigInteger m, BigInteger n) {
+		if (m.equals(BigInteger.ZERO)) {
+			return n.add(BigInteger.ONE);
+		} else if (n.equals(BigInteger.ZERO)) {
+			return Ack(m.subtract(BigInteger.ONE), BigInteger.ONE);
+		} else {
+			return Ack(m.subtract(BigInteger.ONE), Ack(m, n.subtract(BigInteger.ONE)));
+		}
+	}
+}
+
+class AckermannWhile extends AckermannBase {
+	private ArrayList<BigInteger> stack = new ArrayList<BigInteger>();
+
+	public AckermannWhile() {
+		super();
+	}
+
+	BigInteger Ack(BigInteger mInput, BigInteger nInput) {
+		BigInteger m;
+		BigInteger n;
+
+		m = new BigInteger(mInput.toByteArray());
+		n = new BigInteger(nInput.toByteArray());
+		while (true) {
+			if (m.equals(BigInteger.ZERO)) {
+				if (stack.size() == 0) {
+					return n.add(BigInteger.ONE);
+				} else {
+					m = stack.remove(stack.size() - 1).subtract(BigInteger.ONE);
+					n = n.add(BigInteger.ONE);
+				}
+			} else if (n.equals(BigInteger.ZERO)) {
+				m = m.subtract(BigInteger.ONE);
+				n = BigInteger.ONE;
+			} else {
+				stack.add(m);
+				n = n.subtract(BigInteger.ONE);
+			}
+		}
+	}
+}
+
+class AckermannRunner {
+	static void Run(String className, BigInteger m, BigInteger n) {
+		long start;
+		long end;
+		BigInteger result;
+		try {
+			Class clazz = Class.forName(className);
+			if (clazz == null) {
+				System.out.println("ERROR");
+				System.out.println("Class \"" + className + "\" not found");
+				return;
+			}
+			System.out.println("start: (new " + className + "()).Ack(" + m.toString(10) + ", " + n.toString(10) + ")");
+			AckermannBase ack = (AckermannBase)clazz.newInstance();
+			start = System.nanoTime();
+			result = ack.Ack(m, n);
+			end = System.nanoTime();
+			System.out.println("result: " + result.toString(10));
+			System.out.println(String.format("%.8f", (double)(end - start) / 1000000000));
+		} catch (Throwable e) {
+			System.out.println("ERROR");
+			System.out.println(e.getMessage());
+			e.printStackTrace();
+		}
+	}
+
+	public static void main(String[] args) {
+		BigInteger m = new BigInteger("4");
+		BigInteger n = new BigInteger("1");
+		Run("Ackermann", m, n);
+		Run("AckermannWhile", m, n);
+	}
+}
+
